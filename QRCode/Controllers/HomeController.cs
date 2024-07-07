c10d8f6d024f4facc5fd9c9f8286af0fae5011c9
Pratikshya <daspratikshya8@gmail.com>
2024-07-08 00:26:27 +0530

    Add project files.

---
+++ QRCode/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using QRCode.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static QRCoder.PayloadGenerator;
+using System.Web.Mvc;
+
+namespace QRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult CreateQRCode()
+        {
+            return View(new QRCodeModel());
+        }
+        [HttpPost]
+        public ActionResult CreateQRCode(QRCodeModel qRCodeModel)
+        {
+            Payload payload = new Url(qRCodeModel.QRCodeText);
+            QRCodeGenerator codeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = codeGenerator.CreateQrCode(payload);
+            QRCoder.PngByteQRCode pngByte = new PngByteQRCode(qRCodeData);
+            var QrByte = pngByte.GetGraphic(20);
+            string base64Url = Convert.ToBase64String(QrByte);
+            qRCodeModel.QRImageUrl = "data:image/png;base64," + base64Url;
+            return View("CreateQRCode",qRCodeModel);
+
+        }
+    }
+}
---
+++ QRCode/Models/QRCodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRCode.Models
+{
+    public class QRCodeModel
+    {
+        public string QRCodeText {  get; set; }
+        public string QRImageUrl { get; set; }
+
+    }
+}
